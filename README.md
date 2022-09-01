ba98641cfb11cd6fe41ae3ece31bb5e6c3834647
Yutaka Catch <yyycatch@gmail.com>
2022-09-01 10:36:20 +0900

    Copy from b-eee / hexa-vue-example1
    https://github.com/b-eee/hexa-vue-example1

---
+++ README.md
@@ -0,0 +1,48 @@
+# hexa-vue-example1
+本リポジトリは、株式会社Hexabaseが提供するhexabase APIを利用したサンプルアプリケーションです。
+
+本アプリケーションは、Vue.jsを使用しています。
+
+## セットアップ
+本アプリケーションを実行するには、以下の手順を実行してください。
+
+
+### ソースコードの取得
+```
+git clone git@github.com:b-eee/hexa-vue-example1.git
+```
+
+### 実行に必要なパッケージの取得
+```
+npm install
+```
+
+## アプリケーションの実行
+```
+npm run serve
+```
+
+
+## 詳細情報
+
+さらに詳しい実行方法は、[Vueサンプルの実行手順](https://github.com/b-eee/hexa-vue-example1/blob/develop/get_start.md)を参照してください。
+
+
+## License
+
+MIT License.
+
+Please see LICENSE file
+
+
+## 関連情報
+
+- [TODOサンプル](https://b-eee.github.io/development_guide/docs/example/todo_simple)
+- [Hexabase 開発ガイド](https://b-eee.github.io/development_guide/)
+- [Hexabase API Document](https://b-eee.github.io/linker-api-doc/)
+- [株式会社Hexabase](https://www.hexabase.com/)
+- [お問い合わせ](https://www.hexabase.com/contact-us/)
+
+
+
+
---
+++ src/constants/datastore.js
@@ -0,0 +1,84 @@
+export const DATASTORE_TITLE = "TODOサンプル";
+
+export const FIELDS = {
+  TITLE: "Title",
+  ASSIGNEES: "Assignee",
+  CATEGORY: "Category",
+  STATUS: "Status",
+  DUEDATE: "DueDate",
+};
+
+export const OPTIONS = {
+  CATEGORY: [
+    {
+      name: "A",
+      value: "categoryA",
+    },
+    {
+      name: "B",
+      value: "categoryB",
+    },
+    {
+      name: "C",
+      value: "categoryC",
+    },
+  ],
+};
+
+export const STATUS_ACTIONS = {
+  // prettier-ignore
+  "新規": [
+    {
+      id: "MoveToAssigned",
+      name: "受け付ける",
+      icon: "mdi-arrow-right-box",
+    },
+  ],
+  // prettier-ignore
+  "受付": [
+    {
+      id: "BackToNew",
+      name: "新規へ戻す",
+      icon: "mdi-arrow-left-box",
+    },
+    {
+      id: "MoveToInProgress",
+      name: "作業中へ進める",
+      icon: "mdi-arrow-right-box",
+    },
+  ],
+  // prettier-ignore
+  "作業中": [
+    {
+      id: "BackToAssigned",
+      name: "受付へ戻す",
+      icon: "mdi-arrow-left-box",
+    },
+    {
+      id: "MoveToInReview",
+      name: "確認へ進める",
+      icon: "mdi-arrow-right-box",
+    },
+  ],
+  // prettier-ignore
+  "確認": [
+    {
+      id: "BackToInProgress",
+      name: "作業中へ戻す",
+      icon: "mdi-arrow-left-box",
+    },
+    {
+      id: "MoveToClosed",
+      name: "完了する",
+      icon: "mdi-arrow-right-box",
+    },
+  ],
+  // prettier-ignore
+  "完了": [
+    {
+      id: "BackToInReview",
+      name: "確認へ戻す",
+      icon: "mdi-arrow-left-box",
+    },
+  ]
+};
